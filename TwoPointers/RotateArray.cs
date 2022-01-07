5c38c98b7d5a0f82ab170849a28ddb07d4a8b19b
Arturo Filio Villa <arturofiliovilla@Arturos-MacBook-Pro.local>
2022-01-07 17:52:35 -0500

    refactored some stuff

--- TwoPointers/RotateArray.cs
+++ TwoPointers/RotateArray.cs
@@ -9,7 +9,8 @@ namespace TwoPointers {
       for(int i = 0; i < nums.Length; i++ ) {
         // using modulus operator allows us to add index plus the place of 
         // the number of charachters rotated i.e i = 0; rotate k = 3 => swapped[3] = nums[0];
-        // once we are at or beyond nums.Length, (where lenght is 7) i = 5, k = 3; i.e 5 + 3 => 8 % 7 = 1;  => swapped[1] = nums[5]
+        // once we are at or beyond nums.Length, 
+        //    (where lenght is 7) i = 5, k = 3; i.e 5 + 3 => 8 % 7 = 1;  => swapped[1] = nums[5]
         // same goes with i = 4 & k = 3; => 7 % 7 = 0 therefore swapped[0] = nums[4];
         swapped[(i+k) % nums.Length] = nums[i]; 
       }
@@ -17,7 +18,7 @@ namespace TwoPointers {
       for(int i = 0; i < swapped.Length; i++) {
         nums[i] = swapped[i];
       }
-      // THIS IS NOT O(1) memory complexity but it will suffice
+    
     }
 
   }
