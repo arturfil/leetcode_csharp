134127865773f9b43269f60ea69702012b9a6301
Arturo Filio Villa <arturofiliovilla@Arturos-MacBook-Pro.local>
2022-01-10 16:12:29 -0500

    added testing an explanation to problem

--- LinkedLists/MergeTwoLists.cs
+++ LinkedLists/MergeTwoLists.cs
@@ -17,3 +17,28 @@ public class MergeTwoLists {
   }
 
 }
+
+/*
+  TESTING
+  var l1 = new ListNode(1);
+  var l1_2 = new ListNode(3);
+  var l1_3 = new ListNode(4);
+  l1.next = l1_2;
+  l1_2.next = l1_3;
+  
+  var l2 = new ListNode(1);
+  var l2_2 = new ListNode(7);
+  var l2_3 = new ListNode(9);
+  l2.next = l2_2;
+  l2_2.next = l2_3;
+
+  var mer = new MergeTwoLists();
+  mer.MergeLists(l1, l2);
+
+  l1.printList(l1);
+
+  EXPLANATION
+  This algorithm is easily done with a recursion
+  You want to assign l1.next or l2.next equal to the function
+  and the function should return a node.
+*/
