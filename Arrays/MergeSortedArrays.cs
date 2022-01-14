abc836493d817997637d53f79ba334e42cd394d6
arturfil <arturfil@hotmail.com>
2022-01-14 01:44:46 -0500

    testing

--- Arrays/MergeSortedArrays.cs
+++ Arrays/MergeSortedArrays.cs
@@ -23,6 +23,7 @@ public class MergeSortedArrays {
 
 /*
   TESTING
+  // add comment for testing
   var merge = new MergeSortedArrays();
   int[] nums1 = new int[] {1,2,3,0,0,0};
   int[] nums2 = new int[] {2,5,6};
