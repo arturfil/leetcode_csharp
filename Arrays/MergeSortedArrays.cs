06fbc918666c5a28fa75718719efe3ba9065f733
Arturo Filio Villa <arturofiliovilla@Arturos-MacBook-Pro.local>
2022-01-14 01:36:27 -0500

    refactored without the namespace brackets

--- Arrays/MergeSortedArrays.cs
+++ Arrays/MergeSortedArrays.cs
@@ -1,22 +1,20 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Arrays {
+namespace Arrays;
 
-  public class MergeSortedArrays {
+public class MergeSortedArrays {
 
-    public void Merge(int[] nums1, int m, int[] nums2, int n) {
-      int p1 = m-1;
-      int p2 = n-1;
+  public void Merge(int[] nums1, int m, int[] nums2, int n) {
+    int p1 = m-1;
+    int p2 = n-1;
 
-      for (int i = m + n - 1; i >= 0; i--) {
-        if (p2 < 0) break; // in the case that the last digit is zero
-        if (p1 >= 0 && nums1[p1] > nums2[p2]) 
-          nums1[i] = nums1[p1--];
-        else
-          nums1[i] = nums2[p2--];
-      }
-
+    for (int i = m + n - 1; i >= 0; i--) {
+      if (p2 < 0) break; // in the case that the last digit is zero
+      if (p1 >= 0 && nums1[p1] > nums2[p2]) 
+        nums1[i] = nums1[p1--];
+      else
+        nums1[i] = nums2[p2--];
     }
 
   }
