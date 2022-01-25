40d64b5681dd6e74a28e91445f222a35ac7cdb53
arturfil <arturfil@hotmail.com>
2022-01-24 22:57:54 -0500

    minor refactor

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using Arrays;
 using BinaryTrees;
 using Graphs;
+using Strings;
 
 namespace review_leetcode;
 
@@ -8,11 +9,9 @@ class Program {
 
   static void Main(string[] args) {
 
-    string[] lasers = new string[] {"011001","000000","010100","001000"}; // 8
-    string[] lasers2 = new string[] {"0001","010100","001000"}; // 4
-
-    var beams = new NumberOfLaserBeams();
-    beams.NumberOfBeams(lasers);
+    string test = "babad";
+    var lng = new LongestPalindromeSubstring();
+    lng.LongestPalindrome(test);
   
   }
 
--- Strings/LongestPalindromeSubstring.cs
+++ Strings/LongestPalindromeSubstring.cs
@@ -2,8 +2,7 @@ using System;
 
 namespace Strings;
 
-  public class LongestPalindromeSubstring {
-
+public class LongestPalindromeSubstring {
   public string LongestPalindrome(string s) {
     if (string.IsNullOrEmpty(s)) return string.Empty;
 
@@ -23,10 +22,8 @@ namespace Strings;
         res = p2;
       }
     }
-
     return res;
   }
-
 
   private string ExpandAroundCenter(string s, int r, int l) {
     while (r >= 0 && l < s.Length && s[r] == s[l]) {
@@ -73,18 +70,17 @@ namespace Strings;
 }
 
 /*
-  // TEST //
+  TEST
   string test = "babad";      
   var lng = new LongestPalindromeSubstring();
   lng.LongestPalindrome(test);
-*/
 
-/*
- One way of solving this problem would be by pointing in the middle, middle will always be palindrome
- Yout could see a situation where you have two different middle characters therefore you would increase one left and/or 
- One right to see if the next character is a valid palindrome
+  EXPLANATION
+  One way of solving this problem would be by pointing in the middle, middle will always be palindrome
+  Yout could see a situation where you have two different middle characters therefore you would increase one left and/or 
+  One right to see if the next character is a valid palindrome
 
- Technique required: sliding window? -> start with index 0, if the next one is the same, then it's a valid palindrome
- iterate to the next character so see if it matches index 0, if so, recurse inwards.
+  Technique required: sliding window? -> start with index 0, if the next one is the same, then it's a valid palindrome
+  iterate to the next character so see if it matches index 0, if so, recurse inwards.
 
 */
