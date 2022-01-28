6f10225db6b76d4c7f63ad065d4db682d727cd6e
arturfil <arturfil@hotmail.com>
2022-01-28 13:19:13 -0500

    revision for first unique character

--- Arrays/FirstUniqueCharacter.cs
+++ Arrays/FirstUniqueCharacter.cs
@@ -6,18 +6,15 @@ public class FirstUniqueCharacter {
     public int FirstUnique(string s) {
         Dictionary<char, int> word = new Dictionary<char, int>();
         foreach(char ch in s) {
-            if (word.ContainsKey(ch)) {
-                word[ch]++;
-            } else {
-                word.Add(ch, 0);
-            }
+            if (word.ContainsKey(ch)) word[ch]++;
+            else word.Add(ch, 0);
         }
+
         for(int i = 0; i < s.Length; i++) {
             if (word[s[i]] == 0) return i;
         }
         return -1;
     }
-
 }
 
 /*
